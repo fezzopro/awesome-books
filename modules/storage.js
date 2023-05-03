8e1c934ac4e12d3eb053041138e00c35d5fdc6e8
Fezzopro <fezzopro@gmail.com>
2023-05-03 02:13:40 +0200

    Add storage module to the application

---
+++ modules/storage.js
@@ -0,0 +1,31 @@
+class Storage {
+  constructor() {
+    this.BOOK_COLLECTION_NAME = 'collectionOfBooks';
+    this.localStorage = localStorage;
+  }
+
+  isLocalStorage() {
+    return this.localStorage.getItem(this.BOOK_COLLECTION_NAME);
+  }
+
+  readLocalStorage() {
+    return JSON.parse(this.localStorage.getItem(this.BOOK_COLLECTION_NAME));
+  }
+
+  createlocalStorage() {
+    return this.localStorage.setItem(this.BOOK_COLLECTION_NAME, JSON.stringify([]));
+  }
+
+  saveToLocalStorage(book) {
+    return this.localStorage.setItem(this.BOOK_COLLECTION_NAME, JSON.stringify([...this.readLocalStorage(), book]));
+  }
+
+  saveAsLocalSorage(books) {
+    return this.localStorage.setItem(this.BOOK_COLLECTION_NAME, JSON.stringify(books));
+  }
+
+  getCollectionName() {
+    return this.BOOK_COLLECTION_NAME;
+  }
+}
+export default new Storage();
