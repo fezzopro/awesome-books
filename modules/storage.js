9e1438ab3499362e7701d7ee5065ec63daa4732c
Fezzopro <fezzopro@gmail.com>
2023-05-03 04:35:21 +0200

    Refactor dtorage modules to use arrow functions

--- modules/storage.js
+++ modules/storage.js
@@ -4,28 +4,28 @@ class Storage {
     this.localStorage = localStorage;
   }
 
-  isLocalStorage() {
+  isLocalStorage = () => {
     return this.localStorage.getItem(this.BOOK_COLLECTION_NAME);
   }
 
-  readLocalStorage() {
+  readLocalStorage = () => {
     return JSON.parse(this.localStorage.getItem(this.BOOK_COLLECTION_NAME));
   }
 
-  createlocalStorage() {
+  createlocalStorage = () => {
     return this.localStorage.setItem(this.BOOK_COLLECTION_NAME, JSON.stringify([]));
   }
 
-  saveToLocalStorage(book) {
+  saveToLocalStorage = (book) => {
     return this.localStorage.setItem(this.BOOK_COLLECTION_NAME,
       JSON.stringify([...this.readLocalStorage(), book]));
   }
 
-  saveAsLocalSorage(books) {
+  saveAsLocalSorage = (books) => {
     return this.localStorage.setItem(this.BOOK_COLLECTION_NAME, JSON.stringify(books));
   }
 
-  getCollectionName() {
+  getCollectionName = () => {
     return this.BOOK_COLLECTION_NAME;
   }
 }
