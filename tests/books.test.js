f634a60d68dbdeca6ea5dd18c32a64ab304a34e2
Fezzopro <fezzopro@gmail.com>
2023-05-15 20:53:26 +0200

    Add boks module unit tests

---
+++ tests/books.test.js
@@ -0,0 +1,30 @@
+import books from "../modules/books";
+import storage from "../modules/storage";
+
+beforeEach(() => {
+  storage.createlocalStorage();
+});
+
+afterEach(() => {
+  localStorage.removeItem(storage.getCollectionName());
+});
+describe('Books Module', () => {
+
+  const mockBook = {
+    title: 'good man',
+    author: 'Bad Habits',
+    id: Math.ceil(Math.random() * 1000)
+  };
+
+  test('books constructor', () => {
+    expect(books.message).toBe('');
+  });
+
+  test('saveBooks', () => {
+    expect(books.saveBooks(mockBook)).toBe('book saved');
+  });
+
+  test('deleteBook', () => {
+    expect(books.deleteBook(mockBook.id)).toBe('book deleted');
+  });
+});
